bde4dfc5de11f8c50a68dbf1ee45ed3c4a5c0ea1
denervalspelizario <denervalspelizario@gmail.com>
2023-10-15 09:23:48 -0300

    update estrutura basica em expo

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import SignIn from './src/pages/Signin';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar 
+        backgroundColor="#1d1d2e"
+        barStyle="light-content" 
+        translucent={false}
+      />
+      <SignIn />
+    </View>
+  );
+}
+
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {  Text, View } from 'react-native';
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>Login Super Pizza</Text>
+    </View>
+  );
+}
+
